88c15abf2b357935d9802f77bc9b889329ba1dd8
asciarra <angelo.sciarra@lastminute.com>
2017-12-21 21:22:49 +0100

    minor change (reformat)

--- src/main/java/com/lastminute/dojo/BuzzTeller.java
+++ src/main/java/com/lastminute/dojo/BuzzTeller.java
@@ -3,6 +3,6 @@ package com.lastminute.dojo;
 public class BuzzTeller implements Teller {
     @Override
     public String speakOf(int number) {
-        return number %5 == 0 ? "Buzz": "";
+        return number % 5 == 0 ? "Buzz" : "";
     }
 }
