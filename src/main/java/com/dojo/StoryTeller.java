452e565556e40f6588d3056e7e21159a924765a4
Angelo Sciarra <angelosciarra@ymail.com>
2021-04-04 23:11:05 +0200

    rewrote StoryTeller combine using flatMap

--- src/main/java/com/dojo/StoryTeller.java
+++ src/main/java/com/dojo/StoryTeller.java
@@ -1,19 +1,15 @@
 package com.dojo;
 
-import io.vavr.Tuple2;
 import io.vavr.control.Option;
-
-import static io.vavr.API.*;
 
 public interface StoryTeller {
     Option<String> speakOf(int number);
 
     default StoryTeller combine(StoryTeller other) {
-        return number -> Match(new Tuple2<>(this.speakOf(number), other.speakOf(number))).of(
-            Case($((t) -> t._1.isDefined() && t._2.isDefined()), (t) -> Option.of(t._1.get() + t._2.get())),
-            Case($((t) -> t._1.isDefined() && t._2.isEmpty()), (t) -> t._1),
-            Case($((t) -> t._1.isEmpty() && t._2.isDefined()), (t) -> t._2),
-            Case($((t) -> t._1.isEmpty() && t._2.isEmpty()), (t) -> Option.none())
-        );
+        return number -> this.speakOf(number)
+                             .flatMap((word) -> other.speakOf(number)
+                                                     .map((otherWord) -> word + otherWord)
+                                                     .orElse(Option.of(word)))
+                             .orElse(other.speakOf(number));
     }
 }
